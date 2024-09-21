747c51743151fd6bcb87dfc08ffdd8b0a8822b83
Timur Aminev <ablearthy@gmail.com>
2024-09-21 14:08:55 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "go-kart lang";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        devTools = with pkgs; [
+          cargo
+        ];
+      in {
+        devShell = pkgs.mkShell ({
+          buildInputs = devTools;
+        });
+      }
+    );
+}
