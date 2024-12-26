fbcace8f0ad8fef00e5d951f3c42c7bb3717768f
isagila <isagila@mail.ru>
2024-12-26 23:53:08 +0300

    finish parse

--- lib/gokart-parser/src/lib.rs
+++ lib/gokart-parser/src/lib.rs
@@ -15,3 +15,35 @@ pub fn parse<'a>(input: &'a str) -> ParseResult<Ast<'a>> {
 
     Ast::parse(&mut lex.peekable())
 }
+
+// TODO: temp
+// fn get_line_col<'a>(input: &'a str, p: &Pos) -> (usize, usize) {
+//     let mut line = 1;
+//     let mut col = 1;
+
+//     for (idx, ch) in input.char_indices() {
+//         if idx == p.idx {
+//             break;
+//         }
+//         col += 1;
+//         if ch == '\n' {
+//             line += 1;
+//             col = 1;
+//         }
+//     }
+
+//     (line, col)
+// }
+
+// pub fn err_info<'a>(input: &'a str, err: &ParseErr) {
+//     match err {
+//         ParseErr::LexError(lex_err) => todo!(),
+//         ParseErr::UnexpectedEnd => todo!(),
+//         ParseErr::UnexpectedToken(_, pos) => {
+//             println!("{err}");
+//             let (line, col) = get_line_col(input, pos);
+//             println!("at {line}:{col}");
+//         }
+//         ParseErr::InvalidInfix(pos) => todo!(),
+//     }
+// }
--- lib/gokart/src/main.rs
+++ lib/gokart/src/main.rs
@@ -1,22 +1,21 @@
+use gokart_parser::parse;
+
 fn main() {
-    // let result = parse_input(
-    //     r#"
-    //     data IntList = Nil | Cons Int IntList
-    //     data Option = None | Some Int
+    let input = r#"
+        data IntList = Nil | Cons Int IntList
+        data Option = None | Some Int
 
-    //     infixl + 5
+        infixl + 5
 
-    //     let f = 5
+        let f = 5
 
-    //     let headOption x = case x of
-    //         | Nil -> None;
-    //         | Cons x _ -> Some x;
+        let headOption x = case x of
+            | Nil -> None;
+            | Cons x _ -> Some x;
 
-    //     let fix f = letrec x = f x; in x
-    // "#,
-    // )
-    // .unwrap();
-    // for x in result.0 {
-    //     println!("{:?}", x);
-    // }
+        let fix f = letrec x = f x; in x
+    "#;
+    let res = parse(input);
+
+    println!("{:?}", res);
 }
