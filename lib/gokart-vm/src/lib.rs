86e305775801e0ac6f50a965ef5b6df04828c1d0
ShaDi777 <dima.shalanov@mail.ru>
2025-01-17 22:32:38 +0300

    add very simple jit

--- lib/gokart-vm/src/lib.rs
+++ lib/gokart-vm/src/lib.rs
@@ -4,6 +4,12 @@ mod ops;
 mod state;
 mod value;
 mod vm;
+mod jit;
 
 pub use gc::GC;
 pub use vm::VM;
+
+pub use jit::Optimization;
+pub use jit::TailCallOptimization;
+pub use jit::DeadCodeElimination;
+pub use jit::ConstantFolding;
